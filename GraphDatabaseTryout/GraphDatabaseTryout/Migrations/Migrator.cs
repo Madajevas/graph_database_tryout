3434dc49b14cc71efdd1551e36d081eb2b6a4463
Tomas Madajevas <tomas.madajevas@live.com>
2023-03-18 08:06:24 +0200

    move migration runner code to separate file

---
+++ GraphDatabaseTryout/GraphDatabaseTryout/Migrations/Migrator.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphDatabaseTryout.Migrations
+{
+    internal class Migrator
+    {
+        public static void Migrate(bool down)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionString:Graph")
+            ?? throw new ArgumentException("Connection string missing");
+
+            using var services = new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSqlServer()
+                    .WithGlobalConnectionString(connectionString)
+                    .ScanIn(typeof(Program).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+            var scope = services.CreateScope();
+            var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+
+            if (down)
+            {
+                runner.MigrateDown(0);
+            }
+            else
+            {
+                runner.MigrateUp();
+            }
+        }
+    }
+}
--- GraphDatabaseTryout/GraphDatabaseTryout/Program.cs
+++ GraphDatabaseTryout/GraphDatabaseTryout/Program.cs
@@ -1,9 +1,7 @@
 ﻿
 using deniszykov.CommandLine;
 
-using FluentMigrator.Runner;
-
-using Microsoft.Extensions.DependencyInjection;
+using GraphDatabaseTryout.Migrations;
 
 class Program
 {
@@ -17,28 +15,7 @@ class Program
 
     public static int Migrate(bool down = false)
     {
-        var connectionString = Environment.GetEnvironmentVariable("ConnectionString:Graph")
-            ?? throw new ArgumentException("Connection string missing");
-
-        using var services = new ServiceCollection()
-            .AddFluentMigratorCore()
-            .ConfigureRunner(rb => rb
-                .AddSqlServer()
-                .WithGlobalConnectionString(connectionString)
-                .ScanIn(typeof(Program).Assembly).For.Migrations())
-            .AddLogging(lb => lb.AddFluentMigratorConsole())
-            .BuildServiceProvider(false);
-        var scope = services.CreateScope();
-        var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
-
-        if (down)
-        {
-            runner.MigrateDown(0);
-        }
-        else
-        {
-            runner.MigrateUp();
-        }
+        Migrator.Migrate(down);
 
         return 0;
     }
